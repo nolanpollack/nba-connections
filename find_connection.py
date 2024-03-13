af063cebc8e5c889a8897224ee90050573ea377e
Nolan Pollack <pollack.n@northeastern.edu>
2024-03-13 14:33:14 -0400

    Better code style

--- find_connection.py
+++ find_connection.py
@@ -1,64 +1,74 @@
 import ast
-import time
 from collections import deque
 
 import pandas as pd
+from flask import Flask
 from nba_api.stats.static import players
 
-# Constants
-start = time.time()
-first_player = 'Scoot Henderson'
-second_player = 'Bill Russel'
+app = Flask(__name__)
 
 visited = set()
+player_data = pd.read_csv('player_data_test.csv', converters={'teammates': lambda x: set(ast.literal_eval(x))},
+                          index_col=0)
 
 
-def main():
-    # Initialize variables
-    player_data = pd.read_csv('player_data_test.csv', converters={'teammates': lambda x: set(ast.literal_eval(x))}, index_col=0)
-    player1, player2 = get_player_ids(first_player, second_player)
-    queue = deque([(player1['id'], [])])
+@app.route('/')
+def find_connection(first_player, second_player):
+    initial, target = get_players(first_player, second_player)
+    queue = deque([(initial['id'], [])])
 
-    start_search(player_data, player2, queue)
+    return start_search(target['id'], queue)
 
-def get_player_ids(p1, p2):
+
+def get_players(p1, p2):
     player1 = players.find_players_by_full_name(p1)[0]
     player2 = players.find_players_by_full_name(p2)[0]
     return player1, player2
 
 
-def start_search(player_data, player2, queue):
+def start_search(target_id, queue):
     while queue:
         player_id, path = queue.popleft()
-        handle_player(player_data, player2, player_id, path, queue)
+        found_path = handle_player(target_id, player_id, path, queue)
+        if found_path is not None:
+            return found_path
     else:
         print('No connection found')
 
 
-def handle_player(player_data, player2, player_id, path, queue):
+def handle_player(target_id, player_id, path, queue):
+    # Check if player has been visited
     if player_id in visited:
         return
     visited.add(player_id)
+
+    # Get teammates of player
     teammates = player_data.loc[player_data.index == player_id, 'teammates']
     if teammates.size == 0:
         return
+
     for teammate in teammates.iloc[0]:
-        teammate_id = teammate[0]
-        if teammate_id in visited:
-            continue
-        # if player_name.size == 0:
-        #     continue
-        # player_name = player_name.iloc[0]
-        # new_path = path + [player_name]
-        new_path = path + [(teammate_id, teammate[1], teammate[2])]
-        if teammate_id == player2['id']:
-            for player in new_path:
-                player_name = player_data.loc[player_data.index == player[0], 'full_name'].iloc[0]
-                print(player_name, player[1], player[2])
-            print("Players searched: ", len(visited))
-            print("Time elapsed: ", time.time() - start)
-            exit()
-        queue.append((teammate_id, new_path))
+        formatted_path = check_teammate(teammate, target_id, path, queue)
+        if formatted_path is not None:
+            return formatted_path
 
 
-main()
+# Check if teammate is the target player. If so, return the path. Else, add the teammate to the queue
+def check_teammate(teammate, target_id, path, queue):
+    teammate_id = teammate[0]
+    if teammate_id in visited:
+        return
+
+    new_path = path + [(teammate_id, teammate[1], teammate[2])]
+    if teammate_id == target_id:
+        return get_formatted_path(new_path)
+
+    queue.append((teammate_id, new_path))
+
+
+def get_formatted_path(unformatted_path):
+    formatted_path = []
+    for player in unformatted_path:
+        player_name = player_data.loc[player_data.index == player[0], 'full_name'].iloc[0]
+        formatted_path.append({"team": player[2], "year": player[1], "player": player_name})
+    return formatted_path
